bccdf2cda91084ba911936bfb192624936304eaf
yash <yash.jack02@gmail.com>
2023-08-27 00:03:55 +0530

    Update sectionWrapper.js

--- src/hoc/sectionWrapper.js
+++ src/hoc/sectionWrapper.js
@@ -8,7 +8,7 @@ const SectionWrapper = (Component, idName) =>
                 variants={staggerContainer()}
                 initial="hidden"
                 whileInView="show"
-                viewport={{once: false, amount: 0.25}}
+                viewport={{once: true, amount: 0.25}}
                 className='sm:px-16 px-6 sm:py-16 py-10 max-w-7xl mx-auto relative z-0'
             >
                 <span className="hash-span" id={idName}>
